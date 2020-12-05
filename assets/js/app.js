59c61e36d4d3323a74c12862dc865ee29d92e7b6
Fitsum A <fitsum@gmail.com>
2020-12-05 22:40:38 +0300

    display each data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,57 @@
-
 //onload
 document.addEventListener("DOMContentLoaded", loadDatas);
 
 
 
 // load data 
-function loadDatas(e)
-{
-  
-  const xhr = new XMLHttpRequest();
- 
-  xhr.open('GET', '/assets/js/myApps.json', true);
+function loadDatas(e) {
 
-  // xhr.setRequestHeader('X-PINGOTHER', 'pingpong');
-  // xhr.setRequestHeader('Content-Type', 'application/json');
+    const xhr = new XMLHttpRequest();
 
-xhr.onload = function()
-{
-  if(xhr.status === 200)
-  {
-    
-    // Parse 
-    const data = JSON.parse(this.responseText);
+    xhr.open('GET', '/assets/js/myApps.json', true);
 
- 
-      for (const property in data ) {
-        console.log(`${property}`);
+    // xhr.setRequestHeader('X-PINGOTHER', 'pingpong');
+    // xhr.setRequestHeader('Content-Type', 'application/json');
 
-        for (const pr in data [property] ) {
-          console.log(`${pr} : ${data [property][pr]}`);
-          console.log(typeof pr);
-          if(pr === "Name")
-           console.log("True");
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+
+            // Parse 
+            const data = JSON.parse(this.responseText);
+
+
+            for (const property in data) {
+                console.log(`${property}`);
+                console.log(`${data [property].Name}`);
+                console.log(`${data [property].Version}`);
+                console.log(`${data [property].Description}`);
+                console.log(`${data [property].Technology}`);
+                console.log(`${data [property].Logo}`);
+
+
+            }
+
+
+
+
+
+            // let output = '';
+            // cu.forEach(cu => {
+            //   output += `
+            // <ul>
+            // <li>ID : ${cu.id}</li>
+            // <li>Name : ${cu.name}</li>
+            // <li>Company : ${cu.company}</li>
+            // <li>phone : ${cu.phone}</li>
+            // </ul>`;
+            // });
+
+
+
+
+            // document.getElementById('customers').innerHTML = output;
         }
-      }
-
-
-   
-
-
-    // let output = '';
-    // cu.forEach(cu => {
-    //   output += `
-    // <ul>
-    // <li>ID : ${cu.id}</li>
-    // <li>Name : ${cu.name}</li>
-    // <li>Company : ${cu.company}</li>
-    // <li>phone : ${cu.phone}</li>
-    // </ul>`;
-    // });
-
-  
-    
-    
-    // document.getElementById('customers').innerHTML = output;
-  }
-}
-xhr.send();
+    }
+    xhr.send();
 
 }
